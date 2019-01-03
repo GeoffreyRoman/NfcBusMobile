5724d6635adb8b17115bd3eb0886ac0b1540db24
Roman Geoffrey <geoffrey.roman@etu.unice.fr>
2019-01-03 19:27:31 +0100

    page map

--- src/pages/map/map.ts
+++ src/pages/map/map.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { Component } from "@angular/core";
+import { IonicPage, NavController, NavParams } from "ionic-angular";
 
- 
 import L from "leaflet";
 
 /**
@@ -13,30 +12,37 @@ import L from "leaflet";
 
 @IonicPage()
 @Component({
-  selector: 'page-map',
-  templateUrl: 'map.html',
+  selector: "page-map",
+  templateUrl: "map.html"
 })
 export class MapPage {
-
   map;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams) {}
 
   ionViewDidLoad() {
     this.loadMap();
   }
 
-  loadMap(){
+  loadMap() {
+    this.map = L.map("mapid");
+    this.map.on("load", this.onMapLoad);
+    this.map.setView([43.710175, 7.261953], 16);
 
-    this.map = L.map('mapid').setView([43.710175, 7.261953], 16);
-
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ2VvZmZyZXktMyIsImEiOiJjam1oaGt4anYxYW53M3V0ODBrY2ptY2wzIn0.m13RSCPj-ck2-ynQ2M2HnA', {
+    "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png";
+    L.tileLayer(
+      "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiZ2VvZmZyZXktMyIsImEiOiJjam1oaGt4anYxYW53M3V0ODBrY2ptY2wzIn0.m13RSCPj-ck2-ynQ2M2HnA",
+      {
         maxZoom: 18,
-        attribution: 'HappyMem',
-        id: 'mapbox.streets'
-    }).addTo(this.map);
+        attribution: "NfcBus",
+        id: "mapbox.streets"
+      }
+    ).addTo(this.map);
+  }
 
-}
-
+  onMapLoad() {
+    if (document.getElementById("mapid")) {
+      document.getElementById("mapid").style.position = "sticky";
+    }
+  }
 }
